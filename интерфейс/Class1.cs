e4235c80ada102fc005309cfe328ec74feb7be76
vladspiridonov <vlad777.96@mail.ru>
2024-02-15 23:52:35 +0300

    Добавьте файлы проекта.

---
+++ интерфейс/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace интерфейс
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+
+        public int SetStep(int x)
+        {
+            step = x;
+            return step;
+        }
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;           
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+
+        public int SetStep(int x)
+        {
+            step = x;
+            return step;
+        }
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+    }
+}
---
+++ интерфейс/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace интерфейс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeomProgression x = new GeomProgression();
+            ArithProgression y = new ArithProgression();
+            y.SetStart(1);
+            y.SetStep(2);
+            for (int i = 0; i < 10; i++)
+            {                
+                Console.WriteLine(y.GetNext());
+            }
+            y.Reset();
+            x.SetStart(1);
+            x.SetStep(2);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(x.GetNext());
+            }
+            x.Reset();
+            Console.WriteLine(x.GetNext());
+
+            Console.ReadKey();
+        }
+    }
+}
